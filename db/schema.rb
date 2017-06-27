1930c6b501c4e1c2a8e99391d1399e589c4a4ec5
Riss <rissgadi@gmail.com>
2017-06-27 16:50:42 -0500

    removed wikis index from wikis along with wikis id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170625215040) do
+ActiveRecord::Schema.define(version: 20170627214840) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -37,12 +37,10 @@ ActiveRecord::Schema.define(version: 20170625215040) do
     t.text     "body"
     t.boolean  "private"
     t.integer  "user_id"
-    t.integer  "wikis_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   add_index "wikis", ["user_id"], name: "index_wikis_on_user_id"
-  add_index "wikis", ["wikis_id"], name: "index_wikis_on_wikis_id"
 
 end
