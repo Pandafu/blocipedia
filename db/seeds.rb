907399f884afe7e3706b2198194721f26d21f84c
Riss <rissgadi@gmail.com>
2017-06-27 23:15:39 -0500

    faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-include Faker
+require 'faker'
 
 5.times do
   User.create!(
